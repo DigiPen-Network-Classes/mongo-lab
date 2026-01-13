4a0c3354a6136b949d94f3255a5f9562de69aad6
Tony Rasa <trasa@meancat.com>
2026-01-13 15:09:19 -0800

    adding exception handling

--- scoresLocal/scoresLocal.js
+++ scoresLocal/scoresLocal.js
@@ -7,27 +7,35 @@ app.use(express.json());
 const allGames = {};
 
 app.get('/scoresLab/games/:gameName/scores', async (req, res) => {
-
-    let game = allGames[req.params.gameName];
-    if (game == undefined) {
-        res.sendStatus(404);
-        return;
+    try {
+        const game = allGames[req.params.gameName];
+        if (game == undefined) {
+            res.sendStatus(404);
+            return;
+        }
+        res.send(game);
+    } catch(error) {
+        console.error('Failed to lookup', error.message);
+        res.status(500).send({error: 'database operation failed'});
     }
-    res.send(game);
 });
 
 app.post('/scoresLab/games/:gameName/scores/:playerName', async (req, res) => {
-
-    let game = allGames[req.params.gameName];
-    if (game == undefined) {
-        game = {
-            name : req.params.gameName,
-            scores : {}
-        };
-        allGames[req.params.gameName] = game;
+    try {
+        let game = allGames[req.params.gameName];
+        if (game == undefined) {
+            game = {
+                name : req.params.gameName,
+                scores : {}
+            };
+            allGames[req.params.gameName] = game;
+        }
+        game.scores[req.params.playerName] = req.body.score;
+        res.send(game);
+    } catch(error) {
+        console.error('Failed to post score', error.message);
+        res.status(500).send({error: 'database operation failed'});
     }
-    game.scores[req.params.playerName] = req.body.score;
-    res.send(game);
 });
 
 app.listen(port, () => {
